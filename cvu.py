8e981c23c2f94bf29f399d68724bc789ba6b44c3
vvanelslande <vvanelslandedev@gmail.com>
2019-10-15 21:01:09 -0500

    init

---
+++ cvu.py
@@ -0,0 +1,81 @@
+# Copyright (C) 2019 Valentin Vanelslande
+# Licensed under GPLv2 or any later version
+# Refer to the license.txt file included.
+
+import io
+import os
+import re
+import shutil
+import subprocess
+import sys
+import tarfile
+
+import github
+import requests
+import tqdm
+
+if sys.platform != 'win32':
+    raise NotImplementedError('Only Windows is supported')
+
+INSTALL_DIR = os.path.join(os.getenv('LOCALAPPDATA'), 'citra-valentin')
+
+print(f'Installation directory: {INSTALL_DIR}')
+
+installed = None
+
+if not os.path.exists(INSTALL_DIR):
+    os.mkdir(INSTALL_DIR)
+else:
+    enabled = list(
+        filter(lambda n: not n.endswith('-disabled'), os.listdir(INSTALL_DIR)))
+
+    if len(enabled) == 1:
+        installed = re.match(pattern='citra-valentin-windows-(.*)',
+                             string=enabled[0]).group(1)
+
+        print(f'Installed: {installed}')
+    elif len(enabled) == 0:
+        print('No installed and enabled version found.')
+    else:
+        subprocess.Popen(['explorer.exe', INSTALL_DIR])
+        print('Multiple enabled versions found. add -disabled to the other versions.')
+        sys.exit(0)
+
+latest = github.Github().get_repo(
+    'vvanelslande/citra').get_latest_release().tag_name
+
+if installed != latest:
+    print(f'Installing {latest}...')
+
+    r = requests.get(
+        f'https://github.com/vvanelslande/citra/releases/download/{latest}/citra-valentin-windows-{latest}.tar.gz', stream=True)
+
+    size = int(r.headers.get('content-length'))
+    t = tqdm.tqdm(total=size, unit='B', unit_scale=True)
+    tgz = io.BytesIO()
+
+    for data in r.iter_content(chunk_size=1024):
+        t.update(len(data))
+        tgz.write(data)
+
+    t.close()
+
+    tgz.seek(0)
+    tar = tarfile.open(fileobj=tgz, mode='r:gz')
+    if installed != None:
+        disabled = os.path.join(
+            INSTALL_DIR, f'citra-valentin-windows-{installed}-disabled')
+        if os.path.exists(disabled):
+            print(
+                f'Directory \'citra-valentin-windows-{installed}-disabled\' already exists, deleting it.')
+            shutil.rmtree(disabled)
+        os.rename(os.path.join(
+            INSTALL_DIR, f'citra-valentin-windows-{installed}'), disabled)
+        print(f'{installed} was disabled (-disabled suffix added)')
+    tar.extractall(INSTALL_DIR)
+else:
+    print('You have the latest version.')
+
+print('Starting...')
+subprocess.Popen(os.path.join(
+    INSTALL_DIR, f'citra-valentin-windows-{latest}', 'citra-valentin-qt.exe'))
