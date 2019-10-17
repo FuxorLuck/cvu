8c29dd8e2c17ceb4ee33138f10e1845b43853401
vvanelslande <vvanelslandedev@gmail.com>
2019-10-17 18:58:33 -0500

    Add Discord RPC; it copies the user directory now.

--- cvu.py
+++ cvu.py
@@ -11,76 +11,102 @@ import subprocess
 import sys
 import tarfile
 
+import pypresence
 import tqdm
 import urllib3
 
-if sys.platform != 'win32':
-    raise NotImplementedError('Only Windows is supported')
+import discord_rpc
 
-INSTALL_DIR = os.getenv('CVU_INSTALL_DIR', os.path.join(
-    os.getenv('LOCALAPPDATA'), 'citra-valentin'))
 
-print(f'Installation directory: {INSTALL_DIR}')
+def main():
+    INSTALL_DIR = os.getenv('CVU_INSTALL_DIR', os.path.join(
+        os.getenv('LOCALAPPDATA'), 'citra-valentin'))
 
-installed = None
+    print(f'Installation directory: {INSTALL_DIR}')
 
-if not os.path.exists(INSTALL_DIR):
-    os.mkdir(INSTALL_DIR)
-else:
-    enabled = list(
-        filter(lambda n: n.startswith('citra-valentin-windows-') and not n.endswith('-disabled'), os.listdir(INSTALL_DIR)))
+    installed = None
 
-    if len(enabled) == 1:
-        installed = re.match(pattern='citra-valentin-windows-(.*)',
-                             string=enabled[0]).group(1)
-
-        print(f'Installed: {installed}')
-    elif len(enabled) == 0:
-        print('No installed and enabled version found.')
+    if not os.path.exists(INSTALL_DIR):
+        os.mkdir(INSTALL_DIR)
     else:
-        subprocess.Popen(['explorer.exe', INSTALL_DIR])
-        print('Multiple enabled versions found. add -disabled to the other versions.')
-        sys.exit(0)
+        enabled = list(
+            filter(lambda n: n.startswith('citra-valentin-windows-') and not n.endswith('-disabled'), os.listdir(INSTALL_DIR)))
 
-http = urllib3.PoolManager()
+        if len(enabled) == 1:
+            installed = re.match(pattern='citra-valentin-windows-(.*)',
+                                 string=enabled[0]).group(1)
 
-r = http.request('GET', 'https://api.github.com/repos/vvanelslande/citra/releases/latest',
-                 headers={'user-agent': f'Citra Valentin Updater on {sys.platform}'})
-latest = json.loads(r.data.decode('utf8'))['tag_name']
+            print(f'Installed: {installed}')
+        elif len(enabled) == 0:
+            print('No installed and enabled version found.')
+        else:
+            subprocess.Popen(['explorer.exe', INSTALL_DIR])
+            print('Multiple enabled versions found. add -disabled to the other versions.')
+            return
 
-if installed != latest:
-    print(f'Installing {latest}...')
+    http = urllib3.PoolManager()
 
-    r = http.request('GET', f'https://github.com/vvanelslande/citra/releases/download/{latest}/citra-valentin-windows-{latest}.tar.gz', headers={
-        'user-agent': f'Citra Valentin Updater on {sys.platform}'}, preload_content=False)
+    r = http.request('GET', 'https://api.github.com/repos/vvanelslande/citra/releases/latest',
+                     headers={'user-agent': f'Citra Valentin updater on {sys.platform}'})
+    latest = json.loads(r.data.decode('utf8'))['tag_name']
 
-    size = int(r.headers['content-length'])
-    t = tqdm.tqdm(total=size, unit='B', unit_scale=True)
-    tgz = io.BytesIO()
+    if installed != latest:
+        print(f'Installing {latest}...')
 
-    for data in r:
-        t.update(len(data))
-        tgz.write(data)
+        r = http.request('GET', f'https://github.com/vvanelslande/citra/releases/download/{latest}/citra-valentin-windows-{latest}.tar.gz', headers={
+            'user-agent': f'Citra Valentin updater on {sys.platform}'}, preload_content=False)
 
-    t.close()
+        size = int(r.headers['content-length'])
+        t = tqdm.tqdm(total=size, unit='B', unit_scale=True)
+        tgz = io.BytesIO()
 
-    tgz.seek(0)
-    tar = tarfile.open(fileobj=tgz, mode='r:gz')
-    if installed != None:
-        disabled = os.path.join(
-            INSTALL_DIR, f'citra-valentin-windows-{installed}-disabled')
-        if os.path.exists(disabled):
-            print(
-                f'Directory \'citra-valentin-windows-{installed}-disabled\' already exists, deleting it.')
-            shutil.rmtree(disabled)
-        os.rename(os.path.join(
-            INSTALL_DIR, f'citra-valentin-windows-{installed}'), disabled)
-        print(f'{installed} was disabled (-disabled suffix added)')
-    tar.extractall(INSTALL_DIR)
-else:
-    print('You have the latest version.')
+        for data in r:
+            t.update(len(data))
+            tgz.write(data)
 
-args = [os.path.join(
-    INSTALL_DIR, f'citra-valentin-windows-{latest}', 'citra-valentin-qt.exe')] + sys.argv[1:]
-print(f'Starting {" ".join(args)}...')
-subprocess.Popen(args)
+        t.close()
+
+        tgz.seek(0)
+        tar = tarfile.open(fileobj=tgz, mode='r:gz')
+        if installed != None:
+            disabled = os.path.join(
+                INSTALL_DIR, f'citra-valentin-windows-{installed}-disabled')
+            if os.path.exists(disabled):
+                print(
+                    f'Directory \'citra-valentin-windows-{installed}-disabled\' already exists, deleting it.')
+                shutil.rmtree(disabled)
+            os.rename(os.path.join(
+                INSTALL_DIR, f'citra-valentin-windows-{installed}'), disabled)
+            print(f'{installed} was disabled (-disabled suffix added)')
+            if os.path.isdir(os.path.join(disabled, 'user')):
+                shutil.copytree(os.path.join(disabled, 'user'), os.path.join(
+                    INSTALL_DIR, f'citra-valentin-windows-{latest}', 'user'))
+                print(f'user directory from {installed} copied to {latest}')
+        tar.extractall(INSTALL_DIR)
+    else:
+        print('You have the latest version.')
+
+    args = [os.path.join(
+        INSTALL_DIR, f'citra-valentin-windows-{latest}', 'citra-valentin-qt.exe')] + sys.argv[1:]
+    print(f'Starting {" ".join(args)}...')
+
+    cv = subprocess.Popen(args)
+
+    try:
+        discord_rpc.connect()
+    except pypresence.exceptions.InvalidPipe:
+        print('Discord not running.')
+
+        while True:
+            try:
+                cv.wait(0.5)
+
+                break
+            except subprocess.TimeoutExpired:
+                pass
+    finally:
+        cv.terminate()
+
+
+if __name__ == '__main__':
+    main()
---
+++ discord_rpc.py
@@ -0,0 +1,72 @@
+import time
+from ctypes import windll
+
+import pypresence
+import win32gui
+
+found_window = False
+cv_closed = False
+old_text = None
+
+
+def parse_title(text):
+    parts = text.split(' | ')
+    if len(parts) == 0:
+        return ('unknown version', None)
+    elif len(parts) == 1:
+        return (parts[0], None)
+    elif len(parts) == 2:
+        return (parts[0], parts[1])
+    else:
+        raise NotImplementedError(f'len(parts) == {len(parts)}')
+
+
+def enum_callback(hwnd, unknown):
+    global cv_closed
+    global old_text
+    global found_window
+
+    text = win32gui.GetWindowText(hwnd)
+
+    if text.startswith('Citra Valentin '):
+        found_window = True
+        start = time.time()
+
+        rpc = pypresence.Presence(633487273413050418)
+        rpc.connect()
+
+        print('Discord RPC: connected.')
+
+        try:
+            while True:
+                if not windll.user32.IsWindow(hwnd):
+                    break
+
+                text = win32gui.GetWindowText(hwnd)
+
+                if text != old_text:
+                    version, game = parse_title(text)
+
+                    rpc.update(large_image='icon', large_text=f'Nintendo 3DS emulator {version}',
+                               state='Idling' if game is None else 'In-game', details=game, start=start)
+
+                    old_text = text
+        finally:
+            cv_closed = True
+
+            rpc.clear()
+            rpc.close()
+
+
+def connect():
+    win32gui.EnumWindows(enum_callback, 0)
+
+    if not found_window:
+        print('Discord RPC: waiting for Citra Valentin to start...')
+
+        while not cv_closed:
+            win32gui.EnumWindows(enum_callback, 0)
+
+
+if __name__ == '__main__':
+    connect()
